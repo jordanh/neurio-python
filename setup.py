37b42a54e965731132222bcbf578124466f2f902
Jordan Husney <jordan.husney@gmail.com>
2015-03-22 16:05:46 -0400

    Moving to setuptools from distutils...

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ import sys
 sys.path.append('.')
 import neurio
 
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 setup(
   name = 'neurio',
   packages = ['neurio'],
   version = str(neurio.__version__),
-  setup_requires = ['requests'],
   description = 'Neurio energy sensor and appliance automation API library',
   author = 'Jordan Husney',
   author_email = 'jordan.husney@gmail.com',
@@ -15,4 +18,5 @@ setup(
   download_url = 'https://github.com/jordanh/neurio-python/tarball/0.2.2',
   keywords = ['neurio', 'iot', 'energy', 'sensor', 'smarthome', 'automation'],
   classifiers = [],
+  install_requires = ['requests'],
 )
